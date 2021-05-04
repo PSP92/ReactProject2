9f075ea3818deb720b8db735bd36de065b503bbc
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 16:57:49 -0700

    are we headed on the right track?

--- src/data/Main.js
+++ src/data/Main.js
@@ -1,15 +1,11 @@
-// import React from "react"
-// import {useEffect, useState} from "react-r"
+import React from "react";
 
-// const searchData= {
-//   main: "https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097",
-// }
-//  export const Main =(props) =>{
-//   const [Main, setMain] = React.useState(null) 
-//    React.useEffect(() => {
-//      console.log(props)
-//    }
-//    )
-//    return <h1>Home</h1>
-//  }
-// export default Main
+const Main= (props) => {
+  return (
+ <div>
+   <h2>Trending Songs Chart</h2>
+ </div>
+  )
+};
+
+export default Main;
