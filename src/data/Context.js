df6ee9171795eaefa3d71b36c00612312c953e30
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 10:32:58 -0700

    this dang api

---
+++ src/data/Context.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Context= () => {
+  return <h1>This is the Context Component</h1>;
+};
+
+export default Context;
--- src/data/Main.js
+++ src/data/Main.js
@@ -1,6 +1,16 @@
 import React from "react";
-
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios'
+// import { Route } from 'react-router-dom'
 const Main= () => {
+  
+
+
+
+
+
+
+
   return <h1>This is the Main Component</h1>;
 };
 
