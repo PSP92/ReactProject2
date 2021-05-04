6274169677137a24cc1bb09a21fc53b6770d0ea6
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 14:00:22 -0700

    welp

--- src/data/Data.js
+++ src/data/Data.js
@@ -3,17 +3,17 @@ import React, { useState, useEffect } from 'react';
 // import axios from 'axios'
 // import { Route } from 'react-router-dom'
 
-const Data= () => {
-  main:"https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097"
-  artist:"http://api.musixmatch.com/ws/1.1/track.search?q_artist=&page_size=10&page=1&s_track_rating=desc&apikey=9b0be27a6f92a80c6753a5b1a9227097"
-  song:"http://api.musixmatch.com/ws/1.1/track.search?q_track&page_size=10&page=1&s_track_rating=desc&apikey=9b0be27a6f92a80c6753a5b1a9227097"
-  };
+const Data= () => {[
+  main:"https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097",
+  artist:"http://api.musixmatch.com/ws/1.1/track.search?q_artist=&page_size=10&page=1&s_track_rating=desc&apikey=9b0be27a6f92a80c6753a5b1a9227097",
+  song:"http://api.musixmatch.com/ws/1.1/track.search?q_track&page_size=10&page=1&s_track_rating=desc&apikey=9b0be27a6f92a80c6753a5b1a9227097",
+]};
 
 
 
 
-export const Main = (props) =>{
-  const [main, setMain]=React.useState(null)
+export const Data= (props) =>{
+  const [data, setData]=React.useState(null)
     React.useEffect(()=>
     console.log(props)
 )
