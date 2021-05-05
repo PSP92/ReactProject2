4f6157e6ec35ef8f6023375d6b9a1f47552de73b
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 17:33:04 -0700

    is this it?

--- src/data/Main.js
+++ src/data/Main.js
@@ -1,26 +1,18 @@
-import {useState,useEffect} from "react"
+import {useState} from "react"
 
 function Main () {
-  const[trendingSongs,setTrendingSongs]= useState([])
   const [tracklist, setTrackList] =useState ([])
    
   const getTrackList = async () => {
     const response = await fetch("https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097")
     const data = await response.json()
     setTrackList(data.results)  
+    console.log(getTrackList)
   }
   
   
-  
-
-  
-  
-  
-  
-  
-  
   return (
- <div className="Main" tracklist={tracklist} trendingSongs={trendingSongs}>
+ <div className="Main" tracklist={tracklist}>
    <h2>Trending Songs Chart</h2>
    
  </div>
