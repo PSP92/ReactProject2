01e6975c0f281b3d9f7ea9ffd459e2096daedb4a
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-05 00:15:22 -0700

    just kidding I got it

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,13 +1,24 @@
-import React from "react";
+import {useEffect, useState} from "react"
 
-const Artist= (props) => {
+function Artist () {
+  const [artistlist, setArtistList] =useState ([])
+   
+  const getArtistList = async () => {
+    const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/artist.search?q_artist=prodigy&page_size=15&apikey=9b0be27a6f92a80c6753a5b1a9227097")
+    const data = await response.json()
+    setArtistList(data.results)  
+  console.log(data)
+  }
+  
+  useEffect (() => {getArtistList()}, [])
+
   return (
- <div>
-   <form>
+    <div className="Artist" artistlist={artistlist}>
+     <form>
      <input type="text"/>
      <input type="submit" value="submit"/>
    </form>
- </div>
+  </div>
   )
 };
 
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,13 +1,23 @@
-import React from "react";
+import {useEffect, useState} from "react"
+function Song () {
+  const [songlist, setSongList] =useState ([])
+   
+  const getSongList = async () => {
+    const response = await fetch("https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track&page_size=10&page=1&s_track_rating=desc&apikey=9b0be27a6f92a80c6753a5b1a9227097")
+    const data = await response.json()
+    setSongList(data.results)  
+  // console.log(data)
+  }
+  
+  useEffect (() => {getSongList()}, [])
 
-const Song= (props) => {
   return (
- <div>
-   <form>
+    <div className="Song" songlist={songlist}>
+     <form>
      <input type="text"/>
      <input type="submit" value="submit"/>
    </form>
- </div>
+  </div>
   )
 };
 
