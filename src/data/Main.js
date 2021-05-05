868240722b4a71760b8247021ffddc1b975a90b9
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 20:03:58 -0700

    added cards

--- src/data/Main.js
+++ src/data/Main.js
@@ -12,12 +12,92 @@ function Main () {
   
   useEffect (() => {getTrackList()}, [])
   return (
- <div className="Main" tracklist={tracklist}>
-   <h2>Trending Songs Chart</h2>
-   <li>
-     {}
-   </li>
- </div>
+    <div className="Main" tracklist={tracklist}>
+    <h2>Trending Songs Chart</h2>
+  
+    {/* used bootstrap to make cards  */}
+    <div className="row">
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Title</h5>
+          <p className="card-text">Artist</p>
+          <a href="#" className="btn btn-primary">Lyrics</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Title</h5>
+          <p className="card-text">Artist</p>
+          <a href="#" className="btn btn-primary">Lyrics</a>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Artist</p>
+        <a href="#" className="btn btn-primary">Lyrics</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Artist</p>
+        <a href="#" className="btn btn-primary">Lyrics</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div className="row">
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Artist</p>
+        <a href="#" className="btn btn-primary">Lyrics</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Artist</p>
+        <a href="#" className="btn btn-primary">Lyrics</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div className="row">
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Artist</p>
+        <a href="#" className="btn btn-primary">Lyrics</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Title</h5>
+        <p className="card-text">Artist</p>
+        <a href="#" className="btn btn-primary">Lyrics</a>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+  </div>
+
   )
 };
 
