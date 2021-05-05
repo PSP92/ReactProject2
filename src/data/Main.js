fea12f8b06da85b4ca56bd08725f1f28b9fe6fab
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 17:15:06 -0700

    this works? maybe?

--- src/data/Main.js
+++ src/data/Main.js
@@ -1,9 +1,28 @@
-import React from "react";
+import {useState,useEffect} from "react"
 
-const Main= (props) => {
+function Main () {
+  const[trendingSongs,setTrendingSongs]= useState([])
+  const [tracklist, setTrackList] =useState ([])
+   
+  const getTrackList = async () => {
+    const response = await fetch("https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097")
+    const data = await response.json()
+    setTrackList(data.results)  
+  }
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
   return (
- <div>
+ <div className="Main" tracklist={tracklist} trendingSongs={trendingSongs}>
    <h2>Trending Songs Chart</h2>
+   
  </div>
   )
 };
