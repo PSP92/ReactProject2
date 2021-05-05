332c26e5c3f3d577ba7caf25ff189de278cbfec0
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-04 23:03:26 -0700

    i broke it again rip

--- src/data/Main.js
+++ src/data/Main.js
@@ -4,7 +4,7 @@ function Main () {
   const [tracklist, setTrackList] =useState ([])
    
   const getTrackList = async () => {
-    const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097")
+    const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=8&country=us&f_has_lyrics=1&apikey=9b0be27a6f92a80c6753a5b1a9227097")
     const data = await response.json()
     setTrackList(data.results)  
   console.log(data)
@@ -16,7 +16,7 @@ function Main () {
     <h2>Trending Songs Chart</h2>
   
     {/* used bootstrap to make cards  */}
-    <div className="row">
+    <div className="row"          >
     <div className="col-sm-6">
       <div className="card">
         <div className="card-body">
