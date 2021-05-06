79e5207672eb816be48d62471b07b8ae356c5ce1
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-06 06:29:01 -0700

    dear lord help

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,23 @@
-import React from "react";
+import {useEffect, useState} from "react"
+function Search () {
+  const [searchlist, setSearchList] =useState ([])
+   
+  const getSearchList = async () => {
+    const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/q_track_artist&apikey=9b0be27a6f92a80c6753a5b1a9227097")
+    const data = await response.json()
+    setSearchList(data.results)  
+  console.log(data)
+  }
+  
+  useEffect (() => {getSearchList()}, [])
 
-const Search= (props) => {
   return (
- <div>
-   <form>
+    <div className="search" searchlist={searchlist}>
+     <form>
      <input type="text"/>
      <input type="submit" value="submit"/>
    </form>
- </div>
+  </div>
   )
 };
 
